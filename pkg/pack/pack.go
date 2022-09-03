cbd10b72cc130c2a96ed6266dfe1a67886fe3ec8
Fab <fabianjarrett@gmail.com>
2022-09-03 20:07:34 +0100

    Use multiple sheets

--- pkg/pack/pack.go
+++ pkg/pack/pack.go
@@ -18,11 +18,12 @@ const (
 )
 
 type Space struct {
-	Free bool
+	Free  bool
+	Sheet int
 	Rectangle
 }
 
-func recycleSpace(spaces []Space, r Rectangle) *Space {
+func getUsedSpace(spaces []Space, r Rectangle) *Space {
 	for i := 0; i < len(spaces); i++ {
 		s := &spaces[i]
 		if !s.Free {
@@ -35,8 +36,9 @@ func recycleSpace(spaces []Space, r Rectangle) *Space {
 }
 
 type Image struct {
-	Name string
-	Rect Rectangle
+	Name  string
+	Rect  Rectangle
+	Sheet int
 	image.Image
 }
 
@@ -76,35 +78,38 @@ func Pack(dir string) {
 		return iImg.Rect.Width*iImg.Rect.Height > jImg.Rect.Width*jImg.Rect.Height
 	})
 
-	spaces := []Space{{Free: true, Rectangle: Rectangle{Width: maxWidth, Height: maxHeight}}}
+	sheetCount := 1
+	spaces := []Space{{Free: true, Sheet: 0, Rectangle: Rectangle{Width: maxWidth, Height: maxHeight}}}
 
 	for i := range images {
-		image := &images[i].Rect
-		spaceFound := false
-		for _, space := range spaces {
-			if space.Free && space.Rectangle.canFit(*image) {
-				spaceFound = true
-				image.X = space.X
-				image.Y = space.Y
-
-				for j := range spaces {
-					overlap := &spaces[j]
-					if overlap.Free && image.overlaps(overlap.Rectangle) {
-						overlap.Free = false
-						for _, r := range overlap.cut(*image) {
-							if recycleSpace(spaces, r) == nil {
-								spaces = append(spaces, Space{Free: true, Rectangle: r})
-							}
-						}
-					}
-				}
-
+		image := &images[i]
+		var space *Space
+		for _, s := range spaces {
+			if s.Free && s.Rectangle.canFit(image.Rect) {
+				space = &s
 				break
 			}
 		}
-		if !spaceFound {
-			log.Fatalf("Failed to find space for image %v (#%d)\n", image, i)
+		if space == nil {
+			space = &Space{Free: true, Sheet: sheetCount, Rectangle: Rectangle{Width: maxWidth, Height: maxHeight}}
+			sheetCount++
+			spaces = append(spaces, *space)
 		}
+
+		image.Rect.X = space.X
+		image.Rect.Y = space.Y
+		image.Sheet = space.Sheet
+
+		for j := range spaces {
+			overlap := &spaces[j]
+			if overlap.Free && image.Rect.overlaps(overlap.Rectangle) {
+				overlap.Free = false
+				for _, r := range overlap.cut(image.Rect) {
+					spaces = append(spaces, Space{Free: true, Sheet: overlap.Sheet, Rectangle: r})
+				}
+			}
+		}
+
 		sort.Slice(spaces, func(i, j int) bool {
 			iSpc, jSpc := spaces[i], spaces[j]
 			return iSpc.X*iSpc.Y+iSpc.Width*iSpc.Height < jSpc.X*jSpc.Y+jSpc.Width*jSpc.Height
@@ -113,26 +118,31 @@ func Pack(dir string) {
 
 	width := maxWidth
 	height := maxHeight
-	sheet := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{width, height}})
+	for i := 0; i < sheetCount; i++ {
+		sheet := image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{width, height}})
 
-	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++ {
-			sheet.Set(x, y, color.White)
+		for x := 0; x < width; x++ {
+			for y := 0; y < height; y++ {
+				sheet.Set(x, y, color.White)
+			}
 		}
-	}
 
-	for _, img := range images {
-		pos := image.Point{-img.Rect.X, -img.Rect.Y}
-		draw.Draw(sheet, sheet.Bounds(), img.Image, pos, draw.Src)
-	}
+		for _, img := range images {
+			if img.Sheet != i {
+				continue
+			}
+			pos := image.Point{-img.Rect.X, -img.Rect.Y}
+			draw.Draw(sheet, sheet.Bounds(), img.Image, pos, draw.Src)
+		}
 
-	f, err := os.Create("sheet_001.png")
-	if err != nil {
-		log.Fatalf("Failed to create sheet file: %v", err)
-	}
-	defer f.Close()
-	err = png.Encode(f, sheet)
-	if err != nil {
-		log.Fatalf("Failed to write to sheet file: %v", err)
+		f, err := os.Create(fmt.Sprintf("sheet_%03d.png", i+1))
+		if err != nil {
+			log.Fatalf("Failed to create sheet file: %v", err)
+		}
+		defer f.Close()
+		err = png.Encode(f, sheet)
+		if err != nil {
+			log.Fatalf("Failed to write to sheet file: %v", err)
+		}
 	}
 }
