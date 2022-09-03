136b5b91500daf831f14ba2a0c6af7edf6803096
Fab <fabianjarrett@gmail.com>
2022-09-03 17:54:24 +0100

    Use subtests with rectangle cut func

--- pkg/pack/rectangle_test.go
+++ pkg/pack/rectangle_test.go
@@ -55,76 +55,58 @@ func TestOverlaps(t *testing.T) {
 	}
 }
 
-func TestCutInside(t *testing.T) {
-	r1 := Rectangle{0, 0, 300, 200}
-	r2 := Rectangle{10, 20, 30, 50}
-
-	rects := r1.cut(r2)
-
-	want := []Rectangle{
-		{0, 0, 300, 20},
-		{0, 70, 300, 130},
-		{0, 0, 10, 200},
-		{40, 0, 260, 200},
+func TestCut(t *testing.T) {
+	var tests = []struct {
+		r1, r2 Rectangle
+		want   []Rectangle
+	}{
+		{
+			Rectangle{0, 0, 300, 200},
+			Rectangle{10, 20, 30, 50},
+			[]Rectangle{
+				{0, 0, 300, 20},
+				{0, 70, 300, 130},
+				{0, 0, 10, 200},
+				{40, 0, 260, 200},
+			},
+		},
+		{
+			Rectangle{0, 0, 300, 200},
+			Rectangle{0, 20, 30, 50},
+			[]Rectangle{
+				{0, 0, 300, 20},
+				{0, 70, 300, 130},
+				{30, 0, 270, 200},
+			},
+		},
+		{
+			Rectangle{0, 0, 300, 200},
+			Rectangle{290, 20, 30, 50},
+			[]Rectangle{
+				{0, 0, 300, 20},
+				{0, 70, 300, 130},
+				{0, 0, 290, 200},
+			},
+		},
+		{
+			Rectangle{0, 0, 300, 200},
+			Rectangle{500, 400, 10, 20},
+			[]Rectangle{},
+		},
+		{
+			Rectangle{0, 0, 300, 200},
+			Rectangle{-50, -40, 10, 20},
+			[]Rectangle{},
+		},
 	}
 
-	if !reflect.DeepEqual(rects, want) {
-		t.Errorf("r1.cut(r2) = %v; want %v", rects, want)
-	}
-}
-
-func TestCutTouchingLeftEdge(t *testing.T) {
-	r1 := Rectangle{0, 0, 300, 200}
-	r2 := Rectangle{0, 20, 30, 50}
-
-	rects := r1.cut(r2)
-
-	want := []Rectangle{
-		{0, 0, 300, 20},
-		{0, 70, 300, 130},
-		{30, 0, 270, 200},
-	}
-
-	if !reflect.DeepEqual(rects, want) {
-		t.Errorf("r1.cut(r2) = %v; want %v", rects, want)
-	}
-}
-
-func TestCutOverlappingRightEdge(t *testing.T) {
-	r1 := Rectangle{0, 0, 300, 200}
-	r2 := Rectangle{290, 20, 30, 50}
-
-	rects := r1.cut(r2)
-
-	want := []Rectangle{
-		{0, 0, 300, 20},
-		{0, 70, 300, 130},
-		{0, 0, 290, 200},
-	}
-
-	if !reflect.DeepEqual(rects, want) {
-		t.Errorf("r1.cut(r2) = %v; want %v", rects, want)
-	}
-}
-
-func TestCutOutsideBottomRight(t *testing.T) {
-	r1 := Rectangle{0, 0, 300, 200}
-	r2 := Rectangle{500, 400, 10, 20}
-
-	rects := r1.cut(r2)
-
-	if len(rects) > 0 {
-		t.Errorf("r1.cut(r2) = %v; want []", rects)
-	}
-}
-
-func TestCutOutsideTopLeft(t *testing.T) {
-	r1 := Rectangle{0, 0, 300, 200}
-	r2 := Rectangle{-50, -40, 10, 20}
-
-	rects := r1.cut(r2)
-
-	if len(rects) > 0 {
-		t.Errorf("r1.cut(r2) = %v; want []", rects)
+	for _, tt := range tests {
+		testName := fmt.Sprintf("%v,%v", tt.r1, tt.r2)
+		t.Run(testName, func(t *testing.T) {
+			rects := tt.r1.cut(tt.r2)
+			if !reflect.DeepEqual(rects, tt.want) {
+				t.Errorf("r1.overlaps(r2) = %v; want %v", rects, tt.want)
+			}
+		})
 	}
 }
